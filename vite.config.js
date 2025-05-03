1a43a480690503fa933bbbf4b0c64e8263fe45de
Apu Sutra Dhar <101419595+apucsd@users.noreply.github.com>
2025-05-03 19:07:26 +0600

    vite change

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     host: "168.231.64.178",
-    // host: "10.0.60.210",
     // port: 4173,
   },
 });
-
