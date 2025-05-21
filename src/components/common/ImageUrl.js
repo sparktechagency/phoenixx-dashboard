509b7449276d3d6ef7d4799006014025df295429
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-05-21 21:34:50 +0600

    court offer fixed again

--- src/components/common/ImageUrl.js
+++ src/components/common/ImageUrl.js
@@ -1,8 +1,8 @@
 import { getBaseUrl } from "../../utils/baseUrl";
 
 export const getImageUrl = (path) => {
-  const imaapiPath = "http://api.mehor.com";
-  // const imaapiPath = "http://168.231.64.178:5002";
+  // const imaapiPath = "http://api.mehor.com";
+  const imaapiPath = "http://168.231.64.178:5002";
   // const imaapiPath = "http://10.0.60.126:5000";
   if (!path) {
     return "https://via.placeholder.com/50"; // Return a fallback image
