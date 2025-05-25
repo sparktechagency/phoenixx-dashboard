c8c05f73e33759e1f29e7c38d321686a2ad691c2
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-05-25 18:02:09 +0600

    rakib vai link config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     // host: "168.231.64.178",
-//     host: "10.0.60.210",
-//     port: 4174,
-//   },
-//   preview: {
-//     // host: "168.231.64.178", // or "0.0.0.0" if preferred
-//     host: "0.0.0.0",
-//     port: 4174,
-//     allowedHosts: ["*"],
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -23,7 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "168.231.64.178",
-    // host: "10.0.60.210",
     port: 4175,
     allowedHosts: [
       "dashboard.mehor.com",
@@ -32,7 +13,7 @@ export default defineConfig({
     ],
   },
   preview: {
-    host: "168.231.64.178", // or "0.0.0.0" if preferred
+    host: "168.231.64.178",
     port: 4175,
     allowedHosts: [
       "dashboard.mehor.com",
