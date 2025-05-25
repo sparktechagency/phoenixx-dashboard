6510ad299bfaaa8e5d1acd9f3c0b67660c5ddeca
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-05-25 17:58:19 +0600

    rakib vai link config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,43 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     // host: "168.231.64.178",
+//     host: "10.0.60.210",
+//     port: 4174,
+//   },
+//   preview: {
+//     // host: "168.231.64.178", // or "0.0.0.0" if preferred
+//     host: "0.0.0.0",
+//     port: 4174,
+//     allowedHosts: ["*"],
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    // host: "168.231.64.178",
-    host: "10.0.60.210",
-    port: 4174,
+    host: "168.231.64.178",
+    // host: "10.0.60.210",
+    port: 4175,
+    allowedHosts: [
+      "dashboard.mehor.com",
+      "www.dashboard.mehor.com",
+      "api.mehor.com",
+    ],
   },
   preview: {
-    // host: "168.231.64.178", // or "0.0.0.0" if preferred
-    host: "0.0.0.0",
-    port: 4174,
-    allowedHosts: ["*"],
+    host: "168.231.64.178", // or "0.0.0.0" if preferred
+    port: 4175,
+    allowedHosts: [
+      "dashboard.mehor.com",
+      "www.dashboard.mehor.com",
+      "api.mehor.com",
+    ],
   },
 });
